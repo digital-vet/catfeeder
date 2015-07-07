b8ac4e1d23e62042d268d02454bf4486593d6c42
digital-vet <dan.finnegan@gmail.com>
2015-07-07 23:20:05 +0000

    Updated all files and structure for the tags that arrived today.

--- loadDB.sql
+++ loadDB.sql
@@ -11,7 +11,12 @@ CREATE TABLE rules (name VARCHAR(50), food VARCHAR(50), mintime INTEGER, amount 
 CREATE TABLE log (id VARCHAR(50), date_time VARCHAR(50));
 
 
-INSERT INTO identification VALUES ("78b3c0020031c064b0f31000009000", "Daniel Finnegan", "Dan.jpg");
+INSERT INTO identification VALUES ("ed9dd8e5", "Daniel Finnegan", "Dan.jpg");
+INSERT INTO identification VALUES ("5bb7d8e5", "Ella", "Ella.jpg");
+INSERT INTO identification VALUES ("f581d5e5", "Toonces", "Toonces.jpg");
+INSERT INTO identification VALUES ("f9bdd4e5", "Hobbes", "Hobbes.jpg");
+INSERT INTO identification VALUES ("eaf6d4e5", "Lexi", "Lexi.jpg");
+INSERT INTO identification VALUES ("bad1d5e5", "Meli", "Meli.jpg");
 
 INSERT INTO rules VALUES ("Meli", "Purina Naturals", 60, 0.5);
 INSERT INTO rules VALUES ("Lexi", "Purina Naturals", 60, 0.5);
@@ -20,10 +25,14 @@ INSERT INTO rules VALUES ("Ella", "Purina DH", 60, 0.6667);
 INSERT INTO rules VALUES ("Hobbes", "Purina DH", 60, 0.6667);
 INSERT INTO rules VALUES ("Daniel Finnegan", "Purina Naturals", 60, 0.5);
 
-INSERT INTO log VALUES('78b3c0020031c064b0f31000009000', '0');
+INSERT INTO log VALUES('ed9dd8e5', '0');
+INSERT INTO log VALUES('5bb7d8e5', '0');
+INSERT INTO log VALUES('f581d5e5', '0');
+INSERT INTO log VALUES('f9bdd4e5', '0');
+INSERT INTO log VALUES('eaf6d4e5', '0');
+INSERT INTO log VALUES('bad1d5e5', '0');
 /* INSERT INTO log VALUES('78b3c0020031c064b0f31000009000', strftime('%s','now'));
 
-SELECT name,food FROM rules WHERE name IN 
+SELECT name,food FROM rules WHERE name IN
 	(SELECT name FROM identification WHERE id="78b3c0020031c064b0f31000009000");
 */
-
